5900fe39258dccbb71edc2403c1f4a64206f8902
PawelPavlo <56196658+PawelPavlo@users.noreply.github.com>
2024-10-05 18:45:42 +0300

    Update main.tf

--- task03/main.tf
+++ task03/main.tf
@@ -1,6 +1,7 @@
 provider "azurerm" {
   features {}
-
+  subscription_id = "5b2dce2d-b796-44e5-8f67-cea66f735d9e"
+  tenant_id       = "b41b72d0-4e9f-4c26-8a69-f949f367c91d"
 }
 
 #resource "azurerm_resource_group" "main" {
