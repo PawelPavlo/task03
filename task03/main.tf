0ff6106ee0799a9c226f587ef9875859be57cc05
PawelPavlo <56196658+PawelPavlo@users.noreply.github.com>
2024-10-13 19:56:34 +0300

    Update main.tf

--- task03/main.tf
+++ task03/main.tf
@@ -1,34 +1,49 @@
 provider "azurerm" {
   features {}
-  subscription_id = "5b2dce2d-b796-44e5-8f67-cea66f735d9e"
-  tenant_id       = "b41b72d0-4e9f-4c26-8a69-f949f367c91d"
 }
 
-#resource "azurerm_resource_group" "main" {
-#  name     = var.rg_name
-#  location = var.location
-#  tags     = var.tags
-#}
-
-data "azurerm_resource_group" "main" {
+data "azurerm_resource_group" "existing" {
   name = var.rg_name
 }
 
-resource "azurerm_storage_account" "main_storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = data.azurerm_resource_group.main.name
-  location                 = data.azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = var.storage_account_replication_type
-  tags                     = var.tags
+resource "azurerm_resource_group" "this" {
+  name     = data.azurerm_resource_group.existing.name
+  location = data.azurerm_resource_group.existing.location
+  tags     = var.tags
 }
 
-resource "azurerm_virtual_network" "main_vnet" {
+resource "azurerm_storage_account" "this" {
+  name                            = var.storage_account_name
+  resource_group_name             = azurerm_resource_group.this.name
+  location                        = var.location
+  account_tier                    = "Standard"
+  account_replication_type        = var.storage_account_replication_type
+  https_traffic_only_enabled      = true
+  public_network_access_enabled   = false
+  allow_nested_items_to_be_public = false
+  tags                            = var.tags
+}
+
+resource "azurerm_virtual_network" "this" {
   name                = var.vnet_name
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
   address_space       = var.vnet_address_space
   tags                = var.tags
+}
+
+resource "azurerm_subnet" "public" {
+  name                 = var.public_snet_name
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = var.public_snet_address_prefixes
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = var.private_snet_name
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = var.private_snet_address_prefixes
 }
 
 resource "azurerm_subnet" "public_subnet" {
