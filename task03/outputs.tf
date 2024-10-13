0870a3ba55cdc534f4ff7e2ee55e6c3717d15179
PawelPavlo <56196658+PawelPavlo@users.noreply.github.com>
2024-10-13 19:57:03 +0300

    Update outputs.tf

--- task03/outputs.tf
+++ task03/outputs.tf
@@ -1,19 +1,14 @@
-#output "rg_id" {
-#  description = "ID of the Resource Group"
-#  value       = azurerm_resource_group.main.id
-#}
-
 output "rg_id" {
-  value = data.azurerm_resource_group.main.id
+  description = "The ID of the Resource Group"
+  value       = azurerm_resource_group.this.id
 }
 
-
 output "sa_blob_endpoint" {
-  description = "storage account blob endpoint URL"
-  value       = azurerm_storage_account.main_storage.primary_blob_endpoint
+  description = "The blob endpoint URL of storage account"
+  value       = azurerm_storage_account.this.primary_blob_endpoint
 }
 
 output "vnet_id" {
-  description = "Vnet ID"
-  value       = azurerm_virtual_network.main_vnet.id
+  description = "The ID of the Virtual Network"
+  value       = azurerm_virtual_network.this.id
 }
